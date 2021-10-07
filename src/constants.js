54eaa223a60dc418419162e57f78b54b129d4d56
Sam Dollar <sdollar1@gmail.com>
2021-10-07 17:40:40 -0500

    temporary fix for loading issue

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ import { userName } from './config';
 
 export const API_URL = 'http://api.geonames.org/countryInfoJSON?';
 
-export const USERNAME = userName;
+export const USERNAME = '&username=sg55kg';
